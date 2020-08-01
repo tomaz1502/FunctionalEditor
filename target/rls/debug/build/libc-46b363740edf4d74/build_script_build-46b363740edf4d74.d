e2647c66fe8eac26a7f6fbd3650d07fcb20a9d5e
tomaz1502 <tomgm1502@gmail.com>
2020-08-01 00:26:50 -0300

    ignore target

--- target/rls/debug/build/libc-46b363740edf4d74/build_script_build-46b363740edf4d74.d
+++
@@ -1,5 +0,0 @@
-/home/tomaz/Área de Trabalho/Tom/Rust/FunctionalEditor/target/rls/debug/build/libc-46b363740edf4d74/build_script_build-46b363740edf4d74: /home/tomaz/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.71/build.rs
-
-/home/tomaz/Área de Trabalho/Tom/Rust/FunctionalEditor/target/rls/debug/build/libc-46b363740edf4d74/build_script_build-46b363740edf4d74.d: /home/tomaz/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.71/build.rs
-
-/home/tomaz/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.71/build.rs:
